fe72a1c2a8240990917bc24bd95f33452ecd606c
raymond <raymond@dd0e9695-b195-4be7-bd10-2dea1a65a6b6>
2012-05-31 20:22:52 +0000

    Localisation updates from http://translatewiki.net.
    git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki@115466 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

--- AjaxShowEditors.i18n.php
+++ AjaxShowEditors.i18n.php
@@ -723,11 +723,12 @@ $messages['km'] = array(
 
 /** Korean (한국어)
  * @author Kwj2772
+ * @author 아라
  */
 $messages['ko'] = array(
 	'ajaxshoweditors-desc' => '같은 문서를 편집하고 있는 사용자를 보여 줌',
 	'ajax-se-title' => '현재 편집 중:',
-	'ajax-se-pending' => '갱신하기… (이 상자를 클릭하거나 편집을 시작하십시오)',
+	'ajax-se-pending' => '새로 고치기... (이 상자를 클릭하거나 편집을 시작하세요)',
 	'ajax-se-idling' => '($1초 전)',
 	'ajax-se-pagedoesnotexist' => '문서가 존재하지 않습니다.',
 	'ajax-se-userinvalid' => '오류: 사용자가 잘못되었습니다.',
